4fb7bd3538e46338a39173bd7c62b6cc65a6c68e
Otsile <tsheleotsile@gmail.com>
2023-07-19 20:16:08 +0200

    login page in java

---
+++ pkg0000/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg0000;
+
+/**
+ *
+ * @author 
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //created an object
+      Login frmmain = new Login();
+      frmmain.setVisible(true); // to display jframe on the screen
+    }
+    
+}
